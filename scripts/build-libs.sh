06b8f6e9d62f9fc9f9c3660eeaba7f5718f6034f
Saad Arman <saad_uajk@outlook.com>
2020-02-12 12:12:07 +0500

    Update build-libs.sh

--- scripts/build-libs.sh
+++ scripts/build-libs.sh
@@ -4,13 +4,13 @@ bash build-it.sh
 
 cd ../scripts/
 echo "building PySEAL python wrapper..."
-pip install setuptools
-pip install -r ../SEALPython/requirements.txt
+pip3 install setuptools
+pip3 install -r ../SEALPython/requirements.txt
 git -C ../SEALPython/ clone https://github.com/pybind/pybind11.git
 git -C ../SEALPython/pybind11/ checkout a303c6fc479662fd53eaa8990dbc65b7de9b7deb
 
 echo "installing library"
 cd ../SEALPython/
-python setup.py build_ext -i
+python3 setup.py build_ext -i
 
 echo "...............sucess..............."
